171f55038cf1cc1e44ad4877fd62057ad5640e17
Daniel Roque <droque6@icloud.com>
2023-12-11 00:27:08 -0600

    Finish registration page. connect to databaase

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = express();
+
 const db =  mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -15,6 +16,12 @@ const db =  mysql.createConnection({
 
 const publicDirectory = path.join(__dirname, 'public');
 app.use(express.static(publicDirectory));
+
+//Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false }));
+
+//Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
 app.set('view engine', 'hbs');
 
@@ -26,10 +33,10 @@ db.connect((error) => {
   }
 });
 
-app.get('/', (req, res) => {
-  // res.send('<h1>Home Page</h1>');
-  res.render('index');
-});
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
 
 app.listen(4000, () => {
     console.log('Server started on port 4000');
